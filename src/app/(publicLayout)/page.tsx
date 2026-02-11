8a048bc845d323bfca6374efe548bd94d38d7388
Md Meherab Hossain Munna <mdmeherab657@gamil.com>
2026-02-11 15:32:17 +0600

    fix type error

--- src/app/(publicLayout)/page.tsx
+++ src/app/(publicLayout)/page.tsx
@@ -4,7 +4,14 @@ import React from 'react';
 export default function HomePage() {
   return (
     <div>
-      <Hero1></Hero1>
+      <Hero1
+        heading="Build Your Agency Website"
+        description="We create modern, scalable and beautiful web applications."
+        image={{
+          src: '/image/banner-image.jpeg',
+          alt: 'Agency Hero Image',
+        }}
+      />
     </div>
   );
 }
